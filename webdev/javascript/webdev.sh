b3ab5ac9e0fb33c61f63865182f92bd9128d3672
André Lehdermann Silveira <80284411+AndreSilveiraAzion@users.noreply.github.com>
2022-09-15 16:56:31 -0300

    Update webdev.sh

--- webdev/javascript/webdev.sh
+++ webdev/javascript/webdev.sh
@@ -69,6 +69,6 @@ case "$1" in
         update_deploy_script;;
 
     build )
-        npx --package=webpack@5.72.0 webpack --config ./azion/webpack.config.js -o ${OUTPUT_DIR} --mode production
+        npx --package=webpack@5.72.0 -- webpack --config ./azion/webpack.config.js -o ${OUTPUT_DIR} --mode production
         echo "{}" > ./args.json;;
 esac
