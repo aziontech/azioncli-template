71c7c49935b62dcd2c64a8179a0e0dc02d58fe10
Maxwel Mazur <52722027+maxwelbm@users.noreply.github.com>
2023-05-11 15:03:41 -0300

    chore: removed the comment about version id

--- webdev/static/function.js
+++ webdev/static/function.js
@@ -8,7 +8,7 @@ addEventListener('fetch', event => {
 async function handleEvent(event) {
     try {
         const request_path = new URL(event.request.url).pathname;
-        const version_id = "{{ .versionId }}"; // version id usada no upload
+        const version_id = "{{ .versionId }}";
         const defaultRoute = version_id.concat("/index.html");
         const customRoute = version_id.concat(request_path);
         const asset_url = new URL(request_path === "/" ? defaultRoute : customRoute, "file://");
